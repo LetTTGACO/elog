606f6a5b612733a042e338faa32e108bb7f80f18
1874 <i@1874.cool>
2023-02-02 23:36:00 +0800

    feat(elog): @elog/cli sync --env逻辑调整

--- packages/cli/src/scripts/sync.ts
+++ packages/cli/src/scripts/sync.ts
@@ -1,6 +1,7 @@
 import elog from '@elog/core'
 import path from 'path'
 import * as dotenv from 'dotenv'
+import { out } from '@elog/shared'
 
 const sync = async (customConfigName?: string, envPath?: string) => {
   let filename: string
@@ -17,8 +18,15 @@ const sync = async (customConfigName?: string, envPath?: string) => {
   const configPath = path.resolve(process.cwd(), `${filename}`)
   const config = require(configPath)
   // 加载环境变量
-  envPath = path.resolve(process.cwd(), envPath || '.env')
-  dotenv.config({ override: true, path: envPath })
+  if (envPath) {
+    // 本地模式
+    envPath = path.resolve(process.cwd(), envPath)
+    out.info('环境变量', `已指定读取env文件为：${envPath}`)
+    dotenv.config({ override: true, path: envPath })
+  } else {
+    // 生产模式
+    out.info('环境变量', `未指定env文件，将从系统环境变量中读取`)
+  }
   const ctx = new elog(config)
   await ctx.deploy()
 }
