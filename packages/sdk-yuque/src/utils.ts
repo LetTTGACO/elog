b5690428b31e4eb1ec0da2be231c2e5651e644cd
LetTTGACO <1225751694@qq.com>
2023-06-13 17:38:12 +0800

    fix(sdk-yuque): 去掉unified库及相关依赖

--- packages/sdk-yuque/src/utils.ts
+++ packages/sdk-yuque/src/utils.ts
@@ -1,10 +1,6 @@
 import frontMatter from 'front-matter'
 import moment from 'moment'
 import unified from 'unified'
-import remarkParse from 'remark-parse'
-import remarkGfm from 'remark-gfm'
-import remarkStringify from '@elog/remark-stringify'
-import remarkFrontMatter from 'remark-frontmatter'
 import { DocUnite, GetProps } from './types'
 import rehypeParse from 'rehype-parse'
 import rehypeStringify from 'rehype-stringify'
@@ -63,48 +59,6 @@ export function formatDate(date: Date) {
 }
 
 /**
- * 处理表格中的特殊字符
- * @param tree
- */
-const processTable = (tree: any) => {
-  // 找到type为table的节点
-  for (const node of tree.children) {
-    if (node.type == 'table') {
-      // 找到tableCell子节点
-      for (const tableRow of node.children) {
-        for (const tableCell of tableRow.children) {
-          // 删除节点
-          tableCell.children = tableCell.children.filter((raw: any) => {
-            // 判断是不是br
-            const isBr = raw.type === 'html' && raw.value === '<br />'
-            // 是的话删除这个节点
-            return !isBr
-          })
-        }
-      }
-    }
-  }
-}
-
-/**
- * 处理markdown
- * @param content
- */
-export const processMarkdown = (content: string) => {
-  const processValue = unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkFrontMatter, ['yaml'])
-    .use(() => (tree) => {
-      processTable(tree)
-    })
-    .use(remarkStringify)
-    // 开始同步执行解析
-    .processSync(content)
-  return processValue.contents as string
-}
-
-/**
  * 处理语雀字符串
  */
 export function processMarkdownRaw(raw: string) {
@@ -114,8 +68,6 @@ export function processMarkdownRaw(raw: string) {
   const emptyAnchor = /<a name=\".*?\"><\/a>/g
   const hiddenContent = /<div style="display:none">[\s\S]*?<\/div>/gi
   raw = raw.replace(nul, '').replace(nul1, '').replace(hiddenContent, '').replace(emptyAnchor, '')
-  // 处理markdown
-  // raw = processMarkdown(raw)
   const multiBr = /(<br>[\s\n]){2}/gi
   const multiBrEnd = /(<br \/>[\n]?){2}/gi
   const brBug = /<br \/>/g
