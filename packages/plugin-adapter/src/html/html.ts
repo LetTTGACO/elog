11543b3362e6dd198ac65e4ee862b4b2961b11da
LetTTGACO <1225751694@qq.com>
2023-07-22 22:24:46 +0800

    feat(wordpress): 增加代码高亮

--- packages/plugin-adapter/src/html/html.ts
+++ packages/plugin-adapter/src/html/html.ts
@@ -1,12 +1,35 @@
 import { DocDetail } from '@elog/types'
 import { formatHtml } from '../utils'
-import { marked } from 'marked'
+import { Marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
+import hljs from 'highlight.js'
 
+/**
+ * markdown转html
+ * @param post
+ */
 export function htmlAdapter(post: DocDetail) {
   let { body_html, body } = post
   if (body_html) {
     return formatHtml(body_html)
   } else {
-    return marked(body, { mangle: false, headerIds: false })
+    const marked = new Marked(
+      markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code: string, lang: string) {
+          const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+          return hljs.highlight(code, { language }).value
+        },
+      }),
+    )
+    return marked.parse(body, {
+      mangle: false,
+      headerIds: false,
+      gfm: true,
+      breaks: true,
+      pedantic: false,
+      sanitize: false,
+      smartypants: false,
+    })
   }
 }
