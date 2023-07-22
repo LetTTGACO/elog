6970b802fc38ec497ea93f44a6fd33d8a84f2fae
LetTTGACO <1225751694@qq.com>
2023-07-23 00:35:37 +0800

    feat: sync脚本

--- .sync.sh
+++ .sync.sh
@@ -8,5 +8,6 @@ cnpm sync @elog/sdk-confluence
 cnpm sync @elog/sdk-flowus
 cnpm sync @elog/sdk-notion
 cnpm sync @elog/sdk-yuque
+cnpm sync @elog/sdk-wordpress
 cnpm sync @elog/shared
 cnpm sync @elog/types
