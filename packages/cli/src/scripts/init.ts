625fb813f1b79f37a0662bfa5f4f988be1d46152
LetTTGACO <1225751694@qq.com>
2023-10-12 11:16:59 +0800

    feat(cli): 增加添加env文件到gitignore的提示

--- packages/cli/src/scripts/init.ts
+++ packages/cli/src/scripts/init.ts
@@ -6,6 +6,7 @@ const init = async (configName = 'elog.config.js', envName = '.elog.env') => {
   genConfigFile(configName)
   genEnvFile(envName)
   out.access('初始化', '🎉 初始化成功 🎉')
+  out.warning('！注意！', `请将${envName}文件加入.gitignore，防止密码等信息误提交`)
   out.info('下一步', `配置${configName}和${envName}`)
 }
 
