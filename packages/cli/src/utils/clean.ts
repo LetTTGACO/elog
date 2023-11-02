0cbe7474579fa5b4f6afaa700817408ec6b8c9f8
LetTTGACO <1225751694@qq.com>
2023-11-02 13:15:47 +0800

    feat: rimraf删除优化

--- packages/cli/src/utils/clean.ts
+++ packages/cli/src/utils/clean.ts
@@ -11,10 +11,14 @@ const __cwd = process.cwd()
 export const cleanPost = (postPath: string) => {
   try {
     const dist = path.join(__cwd, postPath)
-    rimraf.sync(dist)
-    out.info('清理文档', dist)
+    rimraf(dist, (error) => {
+      if (error) {
+        out.err(`清理文档失败: ${error.message}`)
+      }
+      out.info('清理文档: ' + dist)
+    })
   } catch (error: any) {
-    out.err(`清理文档失败, ${error.message}`)
+    out.err(`清理文档失败: ${error.message}`)
   }
 }
 
@@ -25,7 +29,7 @@ export const cleanCache = (cachePath: string) => {
   try {
     const dist = path.join(__cwd, cachePath)
     fs.unlinkSync(dist)
-    out.info('清理缓存', dist)
+    out.info('清理缓存: ' + dist)
   } catch (error: any) {
     out.err(`清理缓存失败', ${error.message}`)
   }
@@ -37,9 +41,13 @@ export const cleanCache = (cachePath: string) => {
 export const cleanImages = (imgsPath: string) => {
   try {
     const dist = path.join(__cwd, imgsPath)
-    rimraf.sync(dist)
-    out.info('清理图片', dist)
+    rimraf(dist, (error) => {
+      if (error) {
+        out.err(`清理图片失败: ${error.message}`)
+      }
+      out.info('清理图片: ' + dist)
+    })
   } catch (error: any) {
-    out.err(`清理图片失败, ${error.message}`)
+    out.err(`清理图片失败: ${error.message}`)
   }
 }
