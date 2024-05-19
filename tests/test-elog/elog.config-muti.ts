f74732d58195e51c093af815687ed8e3244f5b02
LetTTGACO <1225751694@qq.com>
2024-05-19 19:46:50 +0800

    feat(test): test config

--- tests/test-elog/elog.config-muti.ts
+++ tests/test-elog/elog.config-muti.ts
@@ -1,20 +1,16 @@
 import { defineConfig } from '@elogx-test/elog';
-import fromYuque from '@elogx-test/plugin-from-yuque';
+import fromYuque from '@elogx-test/plugin-from-yuque-pwd';
+import fromYuqueToken from '@elogx-test/plugin-from-yuque-token';
 import toLocal from '@elogx-test/plugin-to-local';
 import imageLocal from '@elogx-test/plugin-image-local';
 
 export default defineConfig([
   {
-    from: fromYuque({
-      pwd: {
-        username: process.env.YUQUE_USERNAME,
-        password: process.env.YUQUE_PWD,
-        login: process.env.YUQUE_LOGIN,
-        repo: process.env.YUQUE_REPO,
-        onlyPublic: false,
-        linebreak: false,
-        cacheFilePath: 'elog.cache1.json',
-      },
+    from: fromYuqueToken({
+      token: process.env.YUQUE_TOKEN,
+      login: process.env.YUQUE_LOGIN,
+      repo: process.env.YUQUE_REPO,
+      onlyPublic: false,
     }),
     to: [
       toLocal({
@@ -34,15 +30,13 @@ export default defineConfig([
   },
   {
     from: fromYuque({
-      pwd: {
-        username: process.env.YUQUE_USERNAME,
-        password: process.env.YUQUE_PWD,
-        login: process.env.YUQUE_LOGIN,
-        repo: process.env.YUQUE_REPO,
-        onlyPublic: false,
-        linebreak: false,
-        cacheFilePath: 'elog.cache2.json',
-      },
+      username: process.env.YUQUE_USERNAME,
+      password: process.env.YUQUE_PWD,
+      login: process.env.YUQUE_LOGIN,
+      repo: process.env.YUQUE_REPO,
+      onlyPublic: false,
+      linebreak: false,
+      cacheFilePath: 'elog.cache2.json',
     }),
     to: [
       toLocal({
--- tests/test-elog/elog.config.ts
+++ tests/test-elog/elog.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from '@elogx-test/elog';
-import fromYuque from '@elogx-test/plugin-from-yuque';
+import fromYuque from '@elogx-test/plugin-from-yuque-token';
 import toLocal from '@elogx-test/plugin-to-local';
 import imageLocal from '@elogx-test/plugin-image-local';
 
 export default defineConfig({
   from: fromYuque({
-    pwd: {
-      username: process.env.YUQUE_USERNAME,
-      password: process.env.YUQUE_PWD,
-      login: process.env.YUQUE_LOGIN,
-      repo: process.env.YUQUE_REPO,
-      onlyPublic: false,
-      linebreak: false,
-    },
-    token: {
-      token: process.env.YUQUE_TOKEN,
-      login: process.env.YUQUE_LOGIN,
-      repo: process.env.YUQUE_REPO,
-      onlyPublic: false,
-    },
+    token: process.env.YUQUE_TOKEN,
+    login: process.env.YUQUE_LOGIN,
+    repo: process.env.YUQUE_REPO,
+    onlyPublic: false,
   }),
   to: [
     toLocal({
