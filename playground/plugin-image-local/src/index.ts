48c1c20291564d9923816103f8015e3f1c931ac9
LetTTGACO <1225751694@qq.com>
2024-06-15 20:55:26 +0800

    feat(feishu): local client

--- playground/plugin-image-local/src/index.ts
+++ playground/plugin-image-local/src/index.ts
@@ -1,12 +1,12 @@
 import type { IPlugin } from '@elogx-test/elog';
-import ImageLocal from './ImageLocal';
+import ImageClient from './ImageClient';
 import type { ImageLocalConfig } from './types';
 
 export default function imageLocal(options: Partial<ImageLocalConfig>): IPlugin {
   return {
     name: 'image-local',
     transform(docs) {
-      const imageLocal = new ImageLocal(options as ImageLocalConfig, this);
+      const imageLocal = new ImageClient(options as ImageLocalConfig, this);
       return imageLocal.replaceImages(docs);
     },
   };
