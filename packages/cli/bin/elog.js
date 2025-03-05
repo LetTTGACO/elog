8f4ab45d0236c23e6a72864ab30b06c65a5f0186
LetTTGACO <1225751694@qq.com>
2025-03-05 10:11:42 +0800

    feat(cli): 支持使用本地版本的 cli

--- packages/cli/bin/elog.js
+++ packages/cli/bin/elog.js
@@ -5,17 +5,16 @@ if (debugIndex > 0) {
   process.env.DEBUG = 'true'
 }
 
-process.on('exit', (code) => {
-  // 全局监听退出事件
-  // TODO 区分不同的code
-  // eslint-disable-next-line no-console
-  // console.error('请查阅Elog配置文档: https://elog.1874.cool/notion/start')
-})
+const importLocal = require('import-local')
 
-require('./../dist/index.js')
-  .run()
-  .catch((e) => {
-    // eslint-disable-next-line no-console
-    console.error(e)
-    process.exit(1)
-  })
+if (importLocal(__filename)) {
+  console.log('elog', '正在使用项目中的 elog cli')
+} else {
+  require('./../dist/index.js')
+    .run()
+    .catch((e) => {
+      // eslint-disable-next-line no-console
+      console.error(e)
+      process.exit(1)
+    })
+}
