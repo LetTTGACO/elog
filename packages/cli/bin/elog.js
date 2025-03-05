4661f1e53fc16d9dc57a8acfd9c5c4c5729acbe0
LetTTGACO <1225751694@qq.com>
2025-03-05 23:29:11 +0800

    feat(cli): 支持使用本地版本的 cli

--- packages/cli/bin/elog.js
+++ packages/cli/bin/elog.js
@@ -7,7 +7,40 @@ if (debugIndex > 0) {
 
 const importLocal = require('import-local')
 if (importLocal(__filename)) {
-  console.log('elog', '正在使用项目中的 elog cli')
+  function minimalInfoLog(prefix, message) {
+    const stream = process.stderr
+    const green = '\x1b[32m'
+    const magenta = '\x1b[35m'
+    const reset = '\x1b[0m'
+    const useColor = stream.isTTY
+
+    let output = ''
+
+    if (useColor) {
+      output += green // Info level color
+    }
+    output += 'info'
+    if (useColor) {
+      output += reset
+    }
+
+    if (prefix) {
+      output += ' '
+      if (useColor) {
+        output += magenta // Prefix color
+      }
+      output += prefix
+      if (useColor) {
+        output += reset
+      }
+    }
+
+    output += ' ' + message
+
+    stream.write(output + '\n')
+  }
+
+  minimalInfoLog('elog', 'using local version of elog cli')
 } else {
   require('./../dist/index.js')
     .run()
